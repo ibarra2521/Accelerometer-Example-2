02c93c8368e22346acc9c2164f3e4d43759cf2ce
ibarra2521 <ibarra2521@gmail.com>
2016-01-17 08:41:26 -0500

    first version

--- Accelerometer Example 2/ViewController.swift
+++ Accelerometer Example 2/ViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblShake: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            self.lblShake.text = "Yes!!!"
+        }
     }
+
+    @IBAction func clean() {
+        self.lblShake.text = ""
+    }
+
 
 
 }
